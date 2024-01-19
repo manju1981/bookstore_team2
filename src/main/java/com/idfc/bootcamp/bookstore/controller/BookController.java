640231c766fcbc5b1f207d8798adc4352131e583
soumen72 <soumentechie4@gmail.com>
2024-01-20 00:07:32 +0530

    create find and find by id completed

--- src/main/java/com/idfc/bootcamp/bookstore/controller/BookController.java
+++ src/main/java/com/idfc/bootcamp/bookstore/controller/BookController.java
@@ -1,10 +1,12 @@
 package com.idfc.bootcamp.bookstore.controller;
 
 import com.idfc.bootcamp.bookstore.dto.BookDto;
+import com.idfc.bootcamp.bookstore.entity.BookEntity;
 import com.idfc.bootcamp.bookstore.service.BookService;
 import com.idfc.bootcamp.bookstore.util.MapperUtility;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,20 +17,27 @@ import java.util.List;
 public class BookController {
 
     BookService bookService;
+
     @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
     }
     @GetMapping("/find-all")
-    public ResponseEntity<List<BookDto>> listBooks(){
-        return ResponseEntity.ok(MapperUtility.mapList(bookService.fetchAllBooks(),BookDto.class));
+    public ResponseEntity<List<BookDto>> listBooks() {
+        return ResponseEntity.ok(MapperUtility.mapList(bookService.fetchAllBooks(), BookDto.class));
     }
     @PostMapping("/create")
-    public ResponseEntity<BookDto> createBook(@RequestBody @Valid BookDto dto){
+    public ResponseEntity<BookDto> createBook(@RequestBody @Valid BookDto dto) {
         return ResponseEntity.ok(MapperUtility.convertClass(bookService.create(dto), BookDto.class));
     }
     @GetMapping("/fetch/{id}")
-    public ResponseEntity<BookDto> fetchBook(@PathVariable(value = "id") Long id){
+    public ResponseEntity<BookDto> fetchBook(@PathVariable(value = "id") Long id) {
         return ResponseEntity.ok(MapperUtility.convertClass(bookService.findById(id), BookDto.class));
+    }
+    @GetMapping("/find-all-paginated")
+    public ResponseEntity<List<BookDto>> findBooksPageable(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return ResponseEntity.ok(MapperUtility.convertPageToList(bookService.findBooksPageable(page, size), BookDto.class));
     }
 }
