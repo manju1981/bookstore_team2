8773cc1c854c6913de82662a41517b09d73b127c
Soumik Bhatta <soumik.bhatta@idfcbank.com>
2024-01-23 10:38:33 +0530

    T204 | Soumik/Vatsal | Header, Footer UI feedback fix

--- src/main/frontend-ui/bookstoreapp/src/components/Header/Header.style.js
+++ src/main/frontend-ui/bookstoreapp/src/components/Header/Header.style.js
@@ -5,13 +5,18 @@ export const headerStyle = {
     justifyContent: 'space-between',
     padding: '10px',
     margin: '0',
+    position: 'fixed',
+    zIndex: 4,
+    top: 0,
+    left: 0,
+    right: 0,
 
 };
 
 export const logoStyle = {
     marginRight: '10px',
     height: 'auto',
-    width:'50px',
+    width: '50px',
 };
 
 export const textStyle = {
@@ -25,4 +30,3 @@ export const searchContainerStyle = {
     marginLeft: 'auto',
     padding: '5px 15px',
 };
-  
