6f814c6c43910de0c0e18af4813fbe3a226bac60
Soumik Bhatta <soumik.bhatta@idfcbank.com>
2024-01-23 11:02:56 +0530

    T204 | Soumik/Vatsal | Header, Footer semantic ui refactoring

---
+++ src/main/frontend-ui/bookstoreapp/src/components/Footer/Footer.style.js
@@ -0,0 +1,24 @@
+const footerStyle = {
+  display: "flex",
+  backgroundColor: "#262626",
+  flexDirection: "row",
+  alignItems: "center",
+  padding: "20px",
+  bottom: 0,
+  left: 0,
+  right: 0,
+};
+
+const footerTextStyle = {
+  flex: 1,
+  marginLeft: "20px",
+  fontSize: "16px",
+  fontWeight: 600,
+  color: "#6ca12b",
+};
+
+const actionIconsStyle = {
+  flexDirection: "row",
+};
+
+export { footerStyle, footerTextStyle, actionIconsStyle };
--- src/main/frontend-ui/bookstoreapp/src/components/Header/Header.style.js
+++ src/main/frontend-ui/bookstoreapp/src/components/Header/Header.style.js
@@ -1,32 +1,33 @@
-export const headerStyle = {
-    backgroundColor: '#262626',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    padding: '10px',
-    margin: '0',
-    position: 'fixed',
-    zIndex: 4,
-    top: 0,
-    left: 0,
-    right: 0,
-
+const headerStyle = {
+  backgroundColor: "#262626",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  padding: "10px",
+  margin: "0",
+  position: "fixed",
+  zIndex: 4,
+  top: 0,
+  left: 0,
+  right: 0,
 };
 
-export const logoStyle = {
-    marginRight: '10px',
-    height: 'auto',
-    width: '50px',
+const logoStyle = {
+  marginRight: "10px",
+  height: "auto",
+  width: "50px",
 };
 
-export const textStyle = {
-    color: '#20bc54',
-    fontFamily: 'Montserrat, sans-serif',
-    fontWeight: 'bold',
-    fontSize: '2.0em',
+const textStyle = {
+  color: "#20bc54",
+  fontFamily: "Montserrat, sans-serif",
+  fontWeight: "bold",
+  fontSize: "2.0em",
 };
 
-export const searchContainerStyle = {
-    marginLeft: 'auto',
-    padding: '5px 15px',
+const searchContainerStyle = {
+  marginLeft: "auto",
+  padding: "5px 15px",
 };
+
+export { headerStyle, logoStyle, textStyle, searchContainerStyle };
