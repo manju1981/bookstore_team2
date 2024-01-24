c15d15eae71a4ee70482aedfa2f4b99da4ad2147
soumen72 <soumentechie4@gmail.com>
2024-01-24 11:14:14 +0530

    T205 : santosh/soumen : minor filter fix

--- src/main/java/com/idfc/bootcamp/bookstore/repository/BookRepository.java
+++ src/main/java/com/idfc/bootcamp/bookstore/repository/BookRepository.java
@@ -17,13 +17,13 @@ public interface BookRepository extends JpaRepository<BookEntity, Long> {
     Page<BookEntity> findByTitleContainsIgnoreCaseOrAuthorContainsIgnoreCaseOrDescriptionContainsIgnoreCase(String title, String author,String description,Pageable pageable);
     BookEntity findByIdAndAuthor(Long id, String author);
     @Query(value = "SELECT * FROM books b WHERE " +
-            "LOWER(COALESCE(b.title, '')) = LOWER(:fieldValue) AND :fieldName = 'title' OR " +
-            "LOWER(COALESCE(b.author, '')) = LOWER(:fieldValue) AND :fieldName = 'author' OR " +
-            "LOWER(COALESCE(b.description, '')) = LOWER(:fieldValue) AND :fieldName = 'description' OR " +
-            "LOWER(COALESCE(b.book_id, '')) = LOWER(:fieldValue) AND :fieldName = 'bookId' OR " +
-            "LOWER(COALESCE(CAST(b.rating AS VARCHAR), '')) = LOWER(:fieldValue) AND :fieldName = 'rating' OR " +
-            "LOWER(COALESCE(b.img, '')) = LOWER(:fieldValue) AND :fieldName = 'img' OR " +
-            "LOWER(COALESCE(CAST(b.price AS VARCHAR), '')) = LOWER(:fieldValue) AND :fieldName = 'price' OR " +
-            "LOWER(COALESCE(CAST(b.quantity AS VARCHAR), '')) = LOWER(:fieldValue) AND :fieldName = 'quantity'", nativeQuery = true)
+            "(LOWER(COALESCE(b.title, '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'title') OR " +
+            "(LOWER(COALESCE(b.author, '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'author') OR " +
+            "(LOWER(COALESCE(b.description, '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'description') OR " +
+            "(LOWER(COALESCE(b.book_id, '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'bookId') OR " +
+            "(LOWER(COALESCE(CAST(b.rating AS VARCHAR), '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'rating') OR " +
+            "(LOWER(COALESCE(b.img, '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'img') OR " +
+            "(LOWER(COALESCE(CAST(b.price AS VARCHAR), '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'price') OR " +
+            "(LOWER(COALESCE(CAST(b.quantity AS VARCHAR), '')) LIKE LOWER(:fieldValue) || '%' AND :fieldName = 'quantity')", nativeQuery = true)
     List<BookEntity> findByField(@Param("fieldName") String fieldName,@Param("fieldValue") String fieldValue);
 }
